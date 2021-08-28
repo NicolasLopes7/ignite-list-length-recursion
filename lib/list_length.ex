2b5805a620ce8c2f0a0303ce767414589b62a2a7
Nicolas Lopes <nicolaslopesbusiness@gmail.com>
2021-08-28 10:44:04 -0300

    Implemeting recursive count function

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,12 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list), do: count(list, 0)
+
+  defp count([], acc) do
+    acc
+  end
+
+  defp count(list, acc) do
+    acc = acc + 1
+    count(tl(list), acc)
   end
 end
